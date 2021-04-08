d215f4f75dac6270a04937450dc6fde7fbbc395e
Ethan <echung2018@pingry.org>
2021-04-07 23:02:16 -0400

    updated schemas

--- src/main/java/edu/brown/cs/student/foodcrawl/DataStructures/Restaurant.java
+++ src/main/java/edu/brown/cs/student/foodcrawl/DataStructures/Restaurant.java
@@ -7,19 +7,17 @@ import java.util.List;
 public class Restaurant {
   private final String id;
   private final String name;
-  private final Double latitude;
-  private final Double longitude;
+  private final String address;
   private final List<String> tags;
 
-  public Restaurant(String n, Double lat, Double longi, List<String> tags, String inputID) {
+  public Restaurant(String n, String address, List<String> tags, String inputID) {
     this.name = n;
     if (inputID == null) {
       this.id = GenerateHashID.generateUUID();
     } else {
       this.id = inputID;
     }
-    this.latitude = lat;
-    this.longitude = longi;
+    this.address = address;
     this.tags = tags;
   }
 
@@ -31,12 +29,8 @@ public class Restaurant {
     return name;
   }
 
-  public Double getLatitude() {
-    return latitude;
-  }
-
-  public Double getLongitude() {
-    return longitude;
+  public String getAddress() {
+    return address;
   }
 
   public List<String> getTags() {
--- src/main/java/edu/brown/cs/student/foodcrawl/DataStructures/User.java
+++ src/main/java/edu/brown/cs/student/foodcrawl/DataStructures/User.java
@@ -6,15 +6,18 @@ import java.util.List;
 public class User {
 
   private final String username;
+  private final String password;
   private final List<String> following;
   private final List<String> followers;
 
   /**
    * invariant that the username for each user is unique!!
    * @param username username string
+   * @param password string
    */
-  public User(String username) {
+  public User(String username, String password) {
     this.username = username;
+    this.password = password;
     this.following = new ArrayList<>();
     this.followers = new ArrayList<>();
   }
