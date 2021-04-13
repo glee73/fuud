c57a38d6d3f9e4595d671b23af8221c429593f22
Benjamin Fiske <bffiske@gmail.com>
2021-04-12 20:35:40 -0400

    .

--- src/main/java/edu/brown/cs/student/foodcrawl/DBCommands/Encryptor.java
+++ src/main/java/edu/brown/cs/student/foodcrawl/DBCommands/Encryptor.java
@@ -1,7 +1,7 @@
 package edu.brown.cs.student.foodcrawl.DBCommands;
 
 public class Encryptor {
-  public String encrypt (String plaintext) {
+  public String encrypt(String plaintext) {
     return String.valueOf(plaintext.hashCode());
   }
 }
