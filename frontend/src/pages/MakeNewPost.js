e0ab063d608fce6d4aa12cfa7652c17dec7a8cb5
kimcas <60239946+kimcas@users.noreply.github.com>
2021-04-19 20:10:53 -0400

    fixing rating styling

--- frontend/src/pages/MakeNewPost.js
+++ frontend/src/pages/MakeNewPost.js
@@ -7,7 +7,8 @@ function MakeNewPost(props) {
 
     let [searchResult, setSearchResult] = useState(null);
     let [msg, setMsg] = useState(null);
-
+    let [confirmRestaurant, setConfirmRestaurant] = useState(null);
+    let [confirmedStatus, setConfirmed] = useState(false);
     const userName = localStorage.getItem("user");
 
     let history = useHistory();
@@ -44,13 +45,21 @@ function MakeNewPost(props) {
             .then(response => {
                 console.log(response.data["success"]);
                 setSearchResult(response.data["success"]);
+                if (response.data["success"]) {
+                    setConfirmRestaurant(response.data["restaurant"]);
+                }
                 return response.data["success"];
             })
             .catch(function (error) {
                 console.log(error);
             });
     }
+    const confirmRestaurantName = () => {
+        console.log("confirm before");
+        setConfirmed(true);
+        console.log("confirm after");
 
+    }
     function showSearchResult() {
         if (searchResult === null) {
             return;
@@ -63,23 +72,41 @@ function MakeNewPost(props) {
                 </div>
             );
         } else {
-            return (
-                <div className="newPostResult">
-                    Restaurant found! Please continue.
-                </div>);
+            // return (
+            //     <div className="newPostResult">
+            //         Restaurant found! Please continue.
+            //     </div>);
+            console.log(confirmRestaurant);
+            if (confirmRestaurant === null) {
+                return (<p>Unable to find restaurant</p>);
+            } else {
+                console.log("1: " + confirmedStatus);
+                return (
+                    <div className="confirmRestaurant">
+                        <p>{confirmRestaurant.name}</p>
+                        <button className="confirmButton" type="button" onClick={confirmRestaurantName}>confirm</button>
+                        <p>{confirmRestaurant.address}</p>
+                    </div>
+                );
+            }
         }
     }
 
     function makePost(e) {
 
         e.preventDefault();
-
-        if (!searchResult) {
+        console.log("2: " + confirmedStatus);
+        if (!confirmedStatus) {
+            setMsg(<p className="newPostResult"> please confirm restaurant before submission </p>)
+            return;
+        }
+        if (!searchResult && !confirmedStatus) {
             setMsg(<p className="newPostResult"> please enter valid restaurant before submission </p>)
             return;
         }
 
-        let restaurantName = document.getElementById('restaurantName').value;
+        // let restaurantName = document.getElementById('restaurantName').value;
+        let restaurantName = confirmRestaurant.name;
         let text = document.getElementById('caption').value;
         let review = document.getElementById('rating').value;
         let timestamp = new Date().toLocaleString();
@@ -89,7 +116,6 @@ function MakeNewPost(props) {
         const ctx = canvas.getContext('2d')
         ctx.drawImage(img, 0, 0)
         const data = canvas.toDataURL("image/png")
-
         const toSend = {
             "restaurantName": restaurantName,
             "text": text,
@@ -115,6 +141,10 @@ function MakeNewPost(props) {
                 if (response.data["success"]) {
                     history.push('/myprofile');
                 } else {
+                    console.log("3: " + confirmedStatus);
+                    // if (!confirmedStatus) {
+                    //     setMsg(<p className="newPostResult"> please confirm restaurant. </p>)
+                    // }
                     setMsg(<p className="newPostResult"> post failed. please try again. </p>)
                 }
             })
@@ -156,7 +186,7 @@ function MakeNewPost(props) {
                 <div className="step step4">
                     <form className="formStyle caption">
                         <label htmlFor="fileUpload">Upload an image (optional):</label>
-                        <input className={"shadow"} type="file" id="fileUpload" name="fileUpload" onChange={handleImageUpload} required/>
+                        <input className={"shadow"} type="file" id="fileUpload" name="fileUpload" onChange={handleImageUpload} accept="image/jpeg, image/png" required/>
                     </form>
                 </div>
                 {msg}
